fe40174069b096657e8c94588c4d5adde03554e3
PauliusU <48020370+PauliusU@users.noreply.github.com>
2022-07-04 21:48:34 +0300

    wip: add initial scraper configuration

--- src/python-web-scrapper/main.py
+++ src/python-web-scrapper/main.py
@@ -3,4 +3,5 @@ import scraper
 
 if __name__ == '__main__':
     config = config_loader.get_config()
-    scraper.scrape_site(config)
+    share_price_data = scraper.scrape_site(config)
+
--- src/python-web-scrapper/scraper.py
+++ src/python-web-scrapper/scraper.py
@@ -9,56 +9,47 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-def scrape_site(config) -> None:
+def scrape_site(config) -> list:
     """ Scrapes website using provided settings """
     try:
         logging.info(f'Loading scraping configuration (URL, selectors, etc.)')
         base_url = config['scraping']['base_url']
-        pages_to_scrape = config['scraping']['pages_to_scrape']
         page_title_text = config['scraping']['page_title_text']
         page_title_selector = config['scraping']['page_title_selector']
-        selector_title = config['scraping']['selector_title']
         selector_price = config['scraping']['selector_price']
 
         project_root = Path(__file__).parent.parent.parent
         driver_path = os.path.join(project_root, 'drivers',
                                    config['scraping']['driver_path'])
 
-        cars = {}
+        prices = []
 
         logging.info('Setting up webdriver')
         service = ChromeService(driver_path)
         options = webdriver.ChromeOptions()
 
         with webdriver.Chrome(service=service, options=options) as driver:
-            for page_index in range(0, pages_to_scrape):
-                url = f'{base_url}{page_index + 1}'
-                logging.info(f'Getting {url}')
-                driver.get(url)
+            logging.info(f'Getting {base_url}')
+            driver.get(base_url)
 
-                logging.info(f'Waiting for the page to load')
-                logging.info(driver.title)
+            logging.info(f'Waiting for the page to load')
+            logging.info(driver.title)
 
-                delay = 20
-                WebDriverWait(driver, delay).until(
-                    expected_conditions.text_to_be_present_in_element(
-                        (By.CSS_SELECTOR, page_title_selector),
-                        page_title_text
-                    )
+            delay = 20
+            WebDriverWait(driver, delay).until(
+                expected_conditions.text_to_be_present_in_element(
+                    (By.CSS_SELECTOR, page_title_selector),
+                    page_title_text
                 )
+            )
 
-                elements_with_titles = driver.find_elements(By.CSS_SELECTOR,
-                                                            selector_title)
-                elements_with_prices = driver.find_elements(By.CSS_SELECTOR,
-                                                            selector_price)
+            elements_with_prices = driver.find_elements(By.CSS_SELECTOR,
+                                                        selector_price)
 
-                for i in range(0, len(elements_with_titles)):
-                    unique_key = f'[{page_index + 1}:{i + 1}] {elements_with_titles[i].text}'
-                    cars[unique_key] = \
-                        elements_with_prices[i].text
+            for i in range(0, len(elements_with_prices)):
+                print(elements_with_prices[i].text)
+                prices.append(elements_with_prices[i].text)
 
-        for car_title in cars:
-            print(f'{car_title}: {cars[car_title]}')
-        logging.info(f'Number of cars: {len(cars)}')
+        return prices
     except Exception as err:
         logging.error(f'Scrapping error {err}')
